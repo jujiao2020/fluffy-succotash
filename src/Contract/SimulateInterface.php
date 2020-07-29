093cd09d0ae5d56b3dbfda2584f6808ffd824489
caozb <1917699115@qq.com>
2020-07-29 17:48:18 +0800

    social media sdk

---
+++ src/Contract/SimulateInterface.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+
+namespace Jcsp\SocialSdk\Contract;
+
+
+use Jcsp\SocialSdk\Model\SimPostVideoParams;
+use Jcsp\SocialSdk\Model\SimPostVideoResult;
+use Jcsp\SocialSdk\Model\SimPostVideoTask;
+
+interface SimulateInterface
+{
+
+    /**
+     * 模拟登录发布视频
+     * @param SimPostVideoParams $params
+     * @return SimPostVideoResult
+     */
+    public function simPostVideo(SimPostVideoParams $params): SimPostVideoResult;
+
+    /**
+     * 查询模拟登录视频发布的任务状态
+     * @param string $taskId
+     * @return SimPostVideoTask
+     */
+    public function queryTaskInfo(string $taskId): SimPostVideoTask;
+
+    /**
+     * 获取社媒发布官方账号列表
+     * @return array
+     */
+    public function getAccountList(): array;
+
+}
